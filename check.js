9056c93f4c6c4c1e539b667c5cc51d9650577ef2
Chad Garrett <chadgarrett_@hotmail.com>
2017-09-09 21:23:48 +0000

    Completed luhnAlgorithm function

--- check.js
+++ check.js
@@ -1,26 +1,31 @@
-/*
- * 
- * 
- * 
- * 
- * 
- * 
- */
 
 function luhnCheck(inputNum) {
     var inputNumArray = inputNum.toString().split("").map(Number);
     var checkDigit = inputNumArray[inputNumArray.length - 1];
     var checkSum = 0;
+    var isEvenPosition = false;
 
-    for (var i = (inputNumArray.length - 1); i <= 1; i--) {
-
+    for (var i = (inputNumArray.length - 1); i >= 0; i--) {
+        if (isEvenPosition) {
+            if ((inputNumArray[i] * 2) > 9) {
+                checkSum += ((inputNumArray[i] * 2) - 9);
+            } else {
+                checkSum += (inputNumArray[i] * 2);
+            }
+        } else {
+            checkSum += inputNumArray[i];
+        }
+        isEvenPosition = !isEvenPosition;
     }
 
+    if (checkSum % 10 === 0) {
+        return true;
+    } else {
+        return false;
+    }
 
-    console.log(inputNumArray);
-    console.log(checkDigit);
-    console.log(checkSum);
 }
 
-luhnCheck(79927398713);
+console.log(luhnCheck(79927398713));  // true
+console.log(luhnCheck(79927398710));  // false
 
